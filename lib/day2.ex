cdd69aba744b8c3492604067eab09d8b4f37b0fc
Jessie Morris <jmorris2@pillartechnology.com>
2019-12-02 23:23:20 -0500

    have to keep working on this tomorrow

--- lib/day2.ex
+++ lib/day2.ex
@@ -1,16 +1,34 @@
 defmodule Day2 do
+  def day2_part2() do
+    initial_program = FileUtil.read_csv_file_to_int_array("day2.txt")
+
+    list_of_results =
+      Enum.map(0..99, fn noun ->
+        Enum.map(0..99, fn verb ->
+          altered_program = List.replace_at(initial_program, 1, noun) |> List.replace_at(2, verb)
+          %{
+            Enum.at(execute(altered_program), 0) =>
+              Integer.to_string(noun) <> ":" <> Integer.to_string(verb)
+          }
+        end)
+      end)
+      |> List.flatten
+
+      Enum.find(list_of_results, fn {x,y} -> x == 19_690_720 end)
+  end
 
   def day2_part1() do
     initial_program = FileUtil.read_csv_file_to_int_array("day2.txt")
     altered_program = List.replace_at(initial_program, 1, 12) |> List.replace_at(2, 2)
     execute(altered_program)
   end
+
   def execute(program) do
     execute_lines(program, 0)
   end
 
   def execute_lines(memory, program_counter) do
-    statement = Enum.slice(memory, program_counter, 4) |> IO.inspect(label: "Executing")
+    statement = Enum.slice(memory, program_counter, 4)
     operation = Enum.at(statement, 0)
 
     new_memory =
@@ -26,16 +44,17 @@ defmodule Day2 do
           second_operand = Enum.at(memory, Enum.at(statement, 2))
           load_address = Enum.at(statement, 3)
           List.replace_at(memory, load_address, first_operand * second_operand)
-        
-        99 -> 
-           IO.inspect(program_counter, label: "Program counter at")
-           memory |> IO.inspect(limit: :infinity)
+
+        99 ->
+          # IO.inspect(program_counter, label: "Program counter at")
+          memory
 
         catch_all ->
           IO.inspect(catch_all, label: "OpCode on raise:")
           IO.inspect(program_counter, label: "Program counter on raise")
           raise("this shouldnt have happened")
       end
+
     if(length(memory) >= program_counter + 4 and Enum.at(memory, program_counter) != 99) do
       execute_lines(new_memory, program_counter + 4)
     else
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -12,14 +12,15 @@ defmodule Day2Test do
   test "can write memory outside of the statement" do
     assert Day2.execute([2, 4, 4, 5, 99, 0]) == [2, 4, 4, 5, 99, 9801]
   end
+
   test "exits program on 99 opcode" do
-    assert Day2.execute([1, 0, 0, 0, 99, 0, 0, 0, 99]) == [2, 0, 0, 0, 99, 0, 0, 0, 99] 
+    assert Day2.execute([1, 0, 0, 0, 99, 0, 0, 0, 99]) == [2, 0, 0, 0, 99, 0, 0, 0, 99]
   end
 
   test "program overwrites memory so in place 99's dont necessarily mean exits" do
-    assert Day2.execute([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+    assert Day2.execute([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
   end
-  
+
   test "executes back to back commands" do
     assert Day2.execute([1, 0, 0, 0, 1, 0, 0, 0, 99]) == [4, 0, 0, 0, 1, 0, 0, 0, 99]
   end
